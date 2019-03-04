72b19e595f43fa2141ddd3fb2aee620db0fc1958
David Hoppenbrouwers <david@ulyssis.org>
2019-03-04 16:54:53 +0100

    IT WORKS YAAAAAY

--- bluetooth-test/bluetooth-test.ino
+++ bluetooth-test/bluetooth-test.ino
@@ -1,11 +1,27 @@
-void setup() {
-  // Comment
-  //nog een comment
-  //nog iets
-	// David's comment
+#include <SoftwareSerial.h>
+
+SoftwareSerial hc06(2,3);
+
+void setup(){
+  //Initialize Serial Monitor
+  Serial.begin(9600);
+  Serial.println("ENTER AT Commands:");
+  //Initialize Bluetooth Serial Port
+  hc06.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}zifjezifojzeoij
+void loop(){
+  //Write data from HC06 to Serial Monitor
+  if (hc06.available()){
+    char c = hc06.read();
+    Serial.write(c);
+    digitalWrite(13, c == '1' ? HIGH : LOW);
+  }
+  
+  //Write from Serial Monitor to HC06
+  if (Serial.available()){
+    char c = Serial.read();
+    hc06.write(c);
+    digitalWrite(13, c == '1' ? HIGH : LOW);
+  }
+}
